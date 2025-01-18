f92e36cc7018c9d0e741b4256423f7f076b0675a
PrimakMV <pm2710@mail.ru>
2025-01-18 23:51:22 +0300

    Added a calculator class

---
+++ src/main/java/com/unittestingpractice/calculator/Calculator.java
@@ -0,0 +1,37 @@
+package com.unittestingpractice.calculator;
+
+import com.unittestingpractice.provider.RandomNumberProviderImpl;
+
+public class Calculator {
+
+    private RandomNumberProviderImpl randomNumberProvider;
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+
+        return a / b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public int addWithRandom(int a) {
+        int random = randomNumberProvider.getRandomNumber();
+        return a + random;
+    }
+}
