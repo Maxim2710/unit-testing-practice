94904add571a2dab5d11f165ad800af8462d7bc7
PrimakMV <pm2710@mail.ru>
2025-01-18 23:32:36 +0300

    Added the project structure

---
+++ src/main/java/com/unit_testing_practice/unit_testing_practice/UnittestingpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.unit_testing_practice.unit_testing_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestingpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestingpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unit_testing_practice/unit_testing_practice/UnittestingpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit_testing_practice.unit_testing_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestingpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
